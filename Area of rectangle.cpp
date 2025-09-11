eabc793c7d960684013736b3baf8dc2d6d76b06e
Uditi1801 <uditi18012008@gmail.com>
2025-09-11 18:26:00 +0530

    Create Area of rectangle.cpp

---
+++ Area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int length,breadth,area;
+    cout<<"enter the length of the rectangle:";
+    cin>>length;
+    cout<<"enter the breadth of the rectangle:";
+    cin>>breadth;
+    area=length*breadth;
+    cout<<"the area of the rectangle is:"<<area<<endl;
+    return 0;
+}
