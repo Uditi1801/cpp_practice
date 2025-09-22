aedcc4383de0c9053d21fb6449caeef13fbc8418
Uditi1801 <uditi18012008@gmail.com>
2025-09-22 10:56:32 +0530

    Create finding_the_largest_and _smallest.cpp

---
+++ finding_the_largest_and _smallest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int smallest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "The largest number is: " << largest << endl;
+    cout << "The smallest number is: " << smallest << endl;
+
+    return 0;
+}
