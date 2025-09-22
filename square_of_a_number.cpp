bc54a753d36c044562e09f5ab91932cb6c4d97fe
Uditi1801 <uditi18012008@gmail.com>
2025-09-22 10:49:17 +0530

    Create square_of_a_number.cpp

---
+++ square_of_a_number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,square;
+    cout<<"enter a number you want to calculate the square of:";
+    cin>>num;
+    square=num*num;
+    cout<<"the sqare of the number is "<<square<<endl;
+    return 0;
+
+}
