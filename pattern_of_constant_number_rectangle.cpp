b6072b496233c9e7b8722cfdda8ddbdbaa076882
Uditi1801 <uditi18012008@gmail.com>
2025-10-07 19:45:16 +0530

    Create pattern_of_constant_number_rectangle.cpp

---
+++ pattern_of_constant_number_rectangle.cpp
@@ -0,0 +1,22 @@
+//pattern of constant numbers rectangle
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n,num;
+    cout<<"enter the number you want the whole pattern to be: ";
+    cin>>num;
+    cout<<"enter the number of rows: ";
+    cin>>m;
+    cout<<"enter the number of columns: ";
+    cin>>n;
+    for(int i=1; i<=m;i++){
+        for(int j=1;j<=n;j++){
+            cout<<num;
+
+        }
+        cout<<endl;
+    }
+
+
+
+}
