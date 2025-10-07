bbdf2e21329793a43624fddcd98d460f12f650ac
Uditi1801 <uditi18012008@gmail.com>
2025-10-07 22:37:02 +0530

    Update inverted_hollow_triangle.cpp

--- inverted_hollow_triangle.cpp
+++ inverted_hollow_triangle.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     int num;
-    cout<<"enter the number of rows you want your rectangle to have:";
+    cout<<"enter the number of rows you want your triangle to have:";
     cin>>num;
     for (int i=1; i<=num; i++){
         for(int j=1; j<=num-i+1;j++){
